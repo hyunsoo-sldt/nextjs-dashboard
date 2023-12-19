2556a05c58c418531f993bb68dc8d09acc594f9c
hyunsoo <cycle.lim@soldout.co.kr>
2023-12-19 16:29:40 +0900

    Course Complete!

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,9 +1,9 @@
 import { Revenue } from './definitions';
 
 export const formatCurrency = (amount: number) => {
-  return (amount / 100).toLocaleString('en-US', {
+  return (amount / 100).toLocaleString('ko-KR', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'KRW',
   });
 };
 
@@ -23,7 +23,7 @@ export const formatDateToLocal = (
 
 export const generateYAxis = (revenue: Revenue[]) => {
   // Calculate what labels we need to display on the y-axis
-  // based on highest record and in 1000s
+  // based on highest record and in the 1000s
   const yAxisLabels = [];
   const highestRecord = Math.max(...revenue.map((month) => month.revenue));
   const topLabel = Math.ceil(highestRecord / 1000) * 1000;
